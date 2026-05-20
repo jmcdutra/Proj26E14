251571a25faaacadd53887c49d9fd999a1a3860a
jmcdutra <jmcamposdutra@gmail.com>
2026-05-20 11:08:00 +0100

    adicionar bibliotecario e gestor, gestor herda do bibliotecario para reaproveitar as permissoes

---
+++ src/bibliotecaupt/Bibliotecario.java
@@ -0,0 +1,8 @@
+package bibliotecaupt;
+
+public class Bibliotecario extends Utilizador {
+
+    public Bibliotecario(String nome, String email, String password) {
+        super(nome, email, password);
+    }
+}
---
+++ src/bibliotecaupt/Gestor.java
@@ -0,0 +1,8 @@
+package bibliotecaupt;
+
+public class Gestor extends Bibliotecario {
+
+    public Gestor(String nome, String email, String password) {
+        super(nome, email, password);
+    }
+}
